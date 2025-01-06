b61f296c6ca58efd685701c4df1e817f11e8088a
fabian <fabbenco97@gmail.com>
2025-01-06 11:49:22 +0100

    Add pagination for data queries

---
+++ HeartSync.Infrastructure/Pagination/PaginationRequest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HeartSync.Infrastructure.Pagination
+{
+    public class PaginationRequest
+    {
+        private int _pageSize = 10;
+
+        public const int MaxPageSize = 100;
+
+        public int PageNumber { get; set; } = 1;
+
+        public int PagesSize
+        {
+            get => _pageSize;
+            set => _pageSize = (value > MaxPageSize) ? MaxPageSize : value;
+        }
+
+    }
+}
---
+++ HeartSync.Infrastructure/Pagination/PaginationResponse.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HeartSync.Infrastructure.Pagination
+{
+    public class PaginationResponse<T>
+    {
+        public int PageNumber { get; set; }
+        public int PageSize { get; set; }
+        public int TotalItems { get; set; }
+        public int TotalPages { get; set; }
+        public List<T> Data { get; set; }
+    
+        public PaginationResponse(int totalItems, int pageNumber, int pageSize, int totalPages, List<T> data)
+        {           
+            TotalItems = totalItems;
+            PageNumber = pageNumber;
+            PageSize = pageSize;
+            TotalPages = (int)Math.Ceiling(totalItems / (double)pageSize);
+            Data = data;
+        }
+    }
+
+}
